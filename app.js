ce28824366b15dab23e44c29e9f24711d56c63b3
Fabrikpz <fabrisilveyra@gmail.com>
2023-05-24 20:44:43 -0300

    termine la lógica de parte de los sliders para que cambiasen los parámetros y solucioné el problema que teniamos con el app.js

--- app.js
+++ app.js
@@ -1,4 +1,101 @@
 /*NO ANDA NADA SI PONES LOS SCRIPTS QUE MODIFIQUEN COSAS DEL EJS ACA NO SE PORQUE VAYA,
 HAY Q PONERLOS EN EL INDEX.EJS LAMENTABLEMENTE, HAY Q PREGUNTARLE A ALGÚN PROFE O NO SE*/
-
 // ok
+/*YA ANDA LE PREGUNTE A CHAT GPT Y ME DIJO Q AGREGARA UNA LINEA DE CÓDIGO
+AL SERVER Y FUNCA*/
+
+let slider1 = document.getElementById("mySlider1");
+let output1 = document.getElementById("sliderValue1");
+
+// SLIDER 1
+slider1.addEventListener("input", function () {
+    output1.innerHTML = this.value;
+});
+
+output1.innerHTML = slider1.value;
+
+let slider2 = document.getElementById("mySlider2");
+let output2 = document.getElementById("sliderValue2");
+
+// SLIDER 2
+slider2.addEventListener("input", function () {
+    output2.innerHTML = this.value;
+});
+
+output2.innerHTML = slider2.value;
+
+let slider3 = document.getElementById("mySlider3");
+let output3 = document.getElementById("sliderValue3");
+
+// SLIDER 3
+slider3.addEventListener("input", function () {
+    output3.innerHTML = this.value;
+});
+
+output3.innerHTML = slider3.value;
+
+let slider4 = document.getElementById("mySlider4");
+let output4 = document.getElementById("sliderValue4");
+
+// SLIDER 4
+slider4.addEventListener("input", function () {
+    output4.innerHTML = this.value;
+});
+
+output4.innerHTML = slider4.value;
+
+let slider5 = document.getElementById("mySlider5");
+let output5 = document.getElementById("sliderValue5");
+
+// SLIDER 5
+slider5.addEventListener("input", function () {
+    output5.innerHTML = this.value;
+});
+
+output5.innerHTML = slider5.value;
+
+
+
+//Lógica para que se guarden los valores modificados de los sliders y poder hacer que la IA de una respuesta de acuerdo a estos.
+const saveValuesBtn = document.getElementById("save-values-btn");
+
+// Función para enviar los valores de los sliders al servidor
+function saveSliderValues() {
+    // Obtener los valores de los sliders
+    const value1 = parseFloat(slider1.value);
+    const value2 = parseInt(slider2.value);
+    const value3 = parseFloat(slider3.value);
+    const value4 = parseFloat(slider4.value);
+    const value5 = parseFloat(slider5.value);
+    
+    // Crea un objeto con los valores de los sliders
+    const sliderValues = {
+        value1,
+        value2,
+        value3,
+        value4,
+        value5
+    };
+    
+    console.log("ya estan guardados")
+    
+    // Realiza una solicitud POST al servidor con los valores de los sliders
+    fetch("/test", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(sliderValues)
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Maneja la respuesta del servidor si es necesario
+        console.log("Respuesta del servidor:", data);
+    })
+    .catch(error => {
+        console.error("Error al enviar los valores de los sliders:", error);
+    });
+}
+
+// Escucha el evento 'click' en el botón para guardar los valores
+saveValuesBtn.addEventListener("click", saveSliderValues);
--- server.js
+++ server.js
@@ -16,6 +16,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
+//Linea agregada al error: "refused to execute script from 'http://localhost:2500/app.js' because its MIME type ('text/html') is not executable, and strict MIME type checking is enabled"
+app.use(express.static('./', {
+	setHeaders: (res, path) => {
+		if (path.endsWith('.js')) {
+			res.setHeader('Content-Type', 'application/javascript');
+		}
+	}
+}));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -44,41 +53,44 @@ let presence_penalty = 0.6;
 const conversationContextPrompt = "";
 
 app.post("/test", (req, res) => {
-	const message = req.body.message;
-	// LLamando a la API de OpenAI
-	openai
-	.createCompletion({
-		model: "text-davinci-003",
-		// Agregamos a la conversación el mensaje en cuestión
-		prompt: conversationContextPrompt + message,
-		temperature,
-		max_tokens,
-		top_p,
-		frequency_penalty,
-		presence_penalty,
-		stop: [" Human:", " AI:"],
-	})
-	.then((response) => {
-		const resp = response.data.choices[0].text;
-		res.render("index", { respuesta: resp });
-	})
-	.catch((error) => {
-		console.error(error);
-		// Manejar el error aquí y enviar una respuesta adecuada al cliente
-	});
+	if (req.body.value1 || req.body.value2 || req.body.value3 || req.body.value4 || req.body.value5) {
+		// Si se actualizan los valores en los sliders, actualiza los parámetros correspondientes en el server
+		temperature = parseFloat(req.body.value1);
+		console.log("nuevo valor de temperature ", temperature);
+		max_tokens = parseInt(req.body.value2);
+		console.log("nuevo valor de max_tokens: ", max_tokens);
+		top_p = parseFloat(req.body.value3);
+		console.log("nuevo valor de top_p ", top_p);
+		frequency_penalty = parseFloat(req.body.value4);
+		console.log("nuevo valor de frequency_penalty: ", frequency_penalty);
+		presence_penalty = parseFloat(req.body.value5);
+		console.log("nuevo valor de presence_penalty: ", presence_penalty);
+		res.json({ message: "Parámetros actualizadas correctamente" });
+	} else {
+		const message = req.body.message;
+		// LLamando a la API de OpenAI
+		openai
+		.createCompletion({
+			model: "text-davinci-003",
+			// Agregamos a la conversación el mensaje en cuestión
+			prompt: conversationContextPrompt + message,
+			temperature,
+			max_tokens,
+			top_p,
+			frequency_penalty,
+			presence_penalty,
+			stop: [" Human:", " AI:"],
+		})
+		.then((response) => {
+			const resp = response.data.choices[0].text;
+			res.render("index", { respuesta: resp });
+		})
+		.catch((error) => {
+			console.error(error);
+			// Manejar el error aquí y enviar una respuesta adecuada al cliente
+		});
+	}
 });
-
-app.post("/test", (req, res) => {
-	const { value1, value2, value3, value4, value5 } = req.body;
-	temperature = parseFloat(value1);
-    max_tokens = parseInt(value2);
-    top_p = parseFloat(value3);
-    frequency_penalty = parseFloat(value4);
-    presence_penalty = parseFloat(value5);
-	res.json({ message: "Respuesta generada con los nuevos parámetros" })
-	console.log("funcionaSupongo")
-});
-
 
 // Escuchamos en el puerto correspondiente
 app.listen(2500, () => {
